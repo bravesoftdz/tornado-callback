8194b3f55da92fb402acb9f902d35bb449855dd5
Johni Douglas Marangon <johni.douglas.marangon@gmail.com.br>
2018-05-10 12:16:35 -0300

    Fisrt commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+RUN pip install -r requirements.txt
+
+COPY . /usr/src/app
+
+CMD [ "python", "-u", "./app.py" ]
---
+++ app.py
@@ -0,0 +1,98 @@
+import tornado
+from tornado.escape import json_decode
+import tornado.web
+import tornado.options
+import os
+import json
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+
+
+class CallbackHandler(tornado.web.RequestHandler):
+    async def post(self):
+
+        authorization = self.request.headers.get('Authorization')
+        print('\033[94mCallback called with authorization', authorization)
+
+        self.set_status(201)
+        self.finish()
+
+
+class GerarCertidaoHandler(tornado.web.RequestHandler):
+
+    async def post(self):
+
+        data = json_decode(self.request.body)
+
+        wait = self.get_argument('wait', '1')
+
+        if wait != '0':
+            self.set_status(201)
+            self.finish(data)
+        else:
+            callback_url = self.get_argument('callbackUrl', '')
+            callback_user = self.get_argument('callbackUser', '')
+            callback_password = self.get_argument('callbackPassword', '')
+
+            import time
+            time.sleep(2)
+
+            client = AsyncHTTPClient()
+
+            request = HTTPRequest(
+                callback_url,
+                method='POST',
+                auth_mode='basic',
+                auth_username=callback_user,
+                auth_password=callback_password,
+                body=json.dumps(data),
+                validate_cert=False,
+            )
+
+            def callback(response):
+                pass
+
+            client.fetch(request, raise_error=True, callback=callback)
+
+            self.set_status(201)
+            self.finish()
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+
+        handlers = [
+            (r"/gerar-certidao", GerarCertidaoHandler),
+            (r"/callback", CallbackHandler)
+        ]
+
+        settings = {
+            "debug": True,
+            "autoreload": True
+        }
+
+        tornado.web.Application.__init__(
+            self, handlers, **settings
+        )
+
+
+app = tornado.httpserver.HTTPServer(Application())
+
+if __name__ == "__main__":
+    try:
+        tornado.options.parse_command_line()
+
+        app.listen(int(os.environ.get("PORT", 9999)))
+
+        print("Tornado on port: %i" % int(os.environ.get("PORT", 9999)))
+
+        io_loop = tornado.ioloop.IOLoop.instance()
+        io_loop.start()
+    except KeyboardInterrupt:
+        exit()
+
+
+'''
+How to use
+
+http://localhost:9999/gerar-certidao?wait=0&callbackUrl=http://0.0.0.0:9999/callback&callbackUser=johnicallbackPassword=1234
+'''
